567868becc7710e1f4c3f2dfa0824fc6702cb59a
oladeindepaul <anuoluwapooladeinde@gmail.com>
2022-10-28 09:04:05 +0100

    thank God

--- mytask/settings.py
+++ mytask/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-pmhsaf4iwdu+1e-6g_6o^(x63ujxn_k=kt1_d3*8a)=9$_zlo+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
