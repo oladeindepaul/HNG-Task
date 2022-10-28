fc782b20855bb12948ef1e03cb602de3e248bb9e
oladeindepaul <anuoluwapooladeinde@gmail.com>
2022-10-28 11:19:01 +0100

    Today is friday

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
 
 @api_view(['GET'])
-def getdata(request):
+def getdata(request, *args, **kwargs):
+    header = {"Access-Control-Allow-Origin":"*"}
     person = {"slackusername" : "Paul", "backend" : True, "age" : "21", "bio" : "Hello, i'm Oladeinde paul . A backend developer and a lover of football"}
-    return Response(person)
+    return JsonResponse(person, headers = header)
--- mytask/settings.py
+++ mytask/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS: True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'mytask.urls'
