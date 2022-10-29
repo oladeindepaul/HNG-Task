c8b52b6b2586365d237e1359ce65b632325b6437
oladeindepaul <anuoluwapooladeinde@gmail.com>
2022-10-29 15:27:03 +0100

    updated slackusername

--- api/views.py
+++ api/views.py
@@ -5,5 +5,5 @@ from django.http import JsonResponse
 @api_view(['GET'])
 def getdata(request, *args, **kwargs):
     header = {"Access-Control-Allow-Origin":"*"}
-    person = {"slackusername" : "Paul", "backend" : True, "age" : 21, "bio" : "Hello, i'm Oladeinde paul . A backend developer and a lover of football"}
+    person = {"slackusername" : "Oladeinde Paul", "backend" : True, "age" : 21, "bio" : "Hello, i'm Oladeinde paul . A backend developer and a lover of football"}
     return JsonResponse(person, headers = header)
